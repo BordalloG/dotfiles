f7f2ca027bfe69c0bada493eaf86fc104df7d13d
bordallog <bordallo.guilherme@hotmail.com>
2023-04-11 23:22:33 -0300

    feat: add shell script to create the symlink from this folder to .config

---
+++ create-symlinks.sh
@@ -0,0 +1,23 @@
+CONFIG_DIR=$HOME/.config
+DOTFILES_DIR=$HOME/dotfiles
+
+function create_dirs() {
+  dir_path=$(dirname "$1")
+  file_name=$(basename "$1")
+
+  # Create directories if they don't exist
+  if [ ! -d "$CONFIG_DIR/$dir_path" ]; then
+    echo "Creating directories for $CONFIG_DIR/$dir_path..."
+    mkdir -p "$CONFIG_DIR/$dir_path"
+  fi
+
+  # Create symlink
+  ln -sf $DOTFILES_DIR/$dir_path/$file_name $CONFIG_DIR/$dir_path/$file_name
+}
+
+create_dirs waybar/config
+create_dirs waybar/style.css
+
+create_dirs hypr/hyprland.conf
+create_dirs hypr/hyprpaper.conf
+
