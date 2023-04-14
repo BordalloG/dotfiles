a9e57c96260843f12b30c4cba5157d2b94f8dbb6
bordallog <bordallo.guilherme@hotmail.com>
2023-04-14 20:48:09 -0300

    chore: quick refactor on symlink script

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,24 +1,40 @@
 CONFIG_DIR=$HOME/.config
 DOTFILES_DIR=$HOME/dotfiles
 
-function create_dirs() {
+function create_in_dot_config() {
   dir_path=$(dirname "$1")
   file_name=$(basename "$1")
 
   # Create directories if they don't exist
   if [ ! -d "$CONFIG_DIR/$dir_path" ]; then
-    echo "Creating directories for $CONFIG_DIR/$dir_path..."
+    echo "[mkdir] $CONFIG_DIR/$dir_path..."
     mkdir -p "$CONFIG_DIR/$dir_path"
   fi
 
-  # Create symlink
-  ln -sf $DOTFILES_DIR/$dir_path/$file_name $CONFIG_DIR/$dir_path/$file_name
+  create_symlink $DOTFILES_DIR/$dir_path/$file_name $CONFIG_DIR/$dir_path/$file_name
 }
 
-create_dirs waybar/config
-create_dirs waybar/style.css
+function create_in_home() {
+  create_symlink $DOTFILES_DIR/$1 $HOME/$1
+}
 
-create_dirs hypr/hyprland.conf
-create_dirs hypr/hyprpaper.conf
+function create_symlink() {
+  if [ -e "$2" ]; then
+    rm -f ~/$2
+  fi
 
-create_dirs alacritty/alacritty.yml
+  echo "[symlink - $(basename "$1") ] $(dirname "$1") <- $(dirname "$2")"
+
+  ln -sf $1 $2
+}
+
+create_in_dot_config waybar/config
+create_in_dot_config waybar/style.css
+
+create_in_dot_config hypr/hyprland.conf
+create_in_dot_config hypr/hyprpaper.conf
+
+create_in_dot_config alacritty/alacritty.yml
+
+create_in_home .oh-my-zsh/themes/bordallog.zsh-theme
+create_in_home .zshrc
